94d6406280ec2a68dca72dabfdb4187ae55db306
Hojoon Ji <sleeperbus@gmail.com>
2014-01-25 10:34:22 +0000

    bitstarter file

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
 var express = require('express');
+var fs = require('fs');
+var buffer = new Buffer(256);
+buffer = fs.readFileSync("index.html");
 var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  response.send(buffer.toString("utf-8"));
 });
 
 var port = process.env.PORT || 8080;
